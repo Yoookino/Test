43aace8462286b5c2b6323cf2aa4a95c171b7cde
Yoookino <45125580+Yoookino@users.noreply.github.com>
2024-06-07 13:37:33 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '修改为自己节点域名'
+      url.hostname = 'www.yoookino.eu.org'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
